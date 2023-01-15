210f4dd291c976bdb266fce5346b01819e1a2bd9
Sushant <93951120+07Sushant@users.noreply.github.com>
2023-01-15 22:43:58 +0530

    Create Missing number in array.py

---
+++ Basics/Missing number in array.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+
+class Solution:
+    def MissingNumber(self,array,n):
+        # code here
+ # code here
+        ans = n*(n+1)/2 - sum(array)
+        return int(ans)
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=Solution().MissingNumber(a,n)
+    print(s)
+# } Driver Code Ends
